002b8b1cf3d7780c26df14d2ddebab866e462ebe
Hoài Tiên05 <bohonuochthihoaitien@gmail.com>
2025-04-21 19:49:59 +1245

    props get data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer';
 import Content from '../Content/Content';
+import { render } from '@testing-library/react';
+import LeftContent from '../Content/LeftContent';
+import RightContent from '../Content/RightContent';
 
 function App() {
-    const NewComponent = () => {}
+    // const NewComponent = () => {}
         return (
     
           <div id="container">
             <Header/>
             {/* this is content area */}
-            <Content/>
+            <LeftContent/>
+            <RightContent/>
             {/* footer content */}
             <Footer/>
+            
           </div>
         );
       }
-
 
 export default App;
